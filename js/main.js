c6f6aa54bc494efb81cc941ac1f3b16a9a62a18d
SELISE\adib.mehedi <adibmehedi@gmail.com>
2017-10-31 21:50:34 +0600

    Input button event listener added

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
-(function(){
+(function () {
     'use strict';
 
-    var vm=this;
+    var vm = this;
     var btnAddInput;
     var btnRemoveInput;
     var btnItemInput;
-    var inputItems=[];
-    var isEditMode=false;
+    var inputFeilds = [];
+    var isEditMode = false;
     var render;
     var events;
     var storage;
-   
 
-    vm.RenderFunctions=function(){
+
+    vm.RenderFunctions = function () {
 
         var createElement = function (tagName, className = null, parentTag = null, id = null, value = null) {
             var elem = document.createElement(tagName);
@@ -23,46 +23,46 @@
             return elem;
         }
 
-        var createButton = function (className, parentTag, id, btnText,value=null) {
+        var createButton = function (className, parentTag, id, btnText, value = null) {
             var elem = createElement('button', className, parentTag, id, value);
             elem.innerText = btnText;
             return elem;
         }
 
-        var createInput = function (className, parentTag, id, placeholder=null) {
+        var createInput = function (className, parentTag, id, placeholder = null) {
             var elem = createElement('input', className, parentTag, id);
             elem.type = "text";
             elem.placeholder = placeholder;
             return elem;
         }
 
-        var disableButton=function(buttonElem){
-            buttonElem.disabled=true;
+        var disableButton = function (buttonElem) {
+            buttonElem.disabled = true;
         }
 
-        var enableButton=function(buttonElem){
-            buttonElem.disabled=false;
+        var enableButton = function (buttonElem) {
+            buttonElem.disabled = false;
         }
 
-        var addInputfeild=function(inputcounter){
-            var item_input_group=vm.getElementById('item-input-group');
-            var inputField=createInput('form-control inputFieldItem',item_input_group,'inputItems','');
+        var addInputfeild = function (inputcounter) {
+            var item_input_group = vm.getElementById('item-input-group');
+            var inputField = createInput('form-control inputFieldItem', item_input_group, 'inputFeilds', '');
         }
 
-        var removeAllInputfeild=function(){
-            var item_input_group=vm.getElementById('item-input-group');
-            item_input_group.innerHTML="";
-         } 
-         
-        var renderInputButtons=function(){
-            var item_input_group=vm.getElementById('item-input-group');
-            var elem=createButton('btn btn-primary btn-block',item_input_group,'btnItemInput','Add Item');
+        var removeAllInputfeild = function () {
+            var item_input_group = vm.getElementById('item-input-group');
+            item_input_group.innerHTML = "";
         }
 
-        var renderInputFeilds=function(){
+        var renderInputButtons = function () {
+            var item_input_group = vm.getElementById('item-input-group');
+            var elem = createButton('btn btn-primary btn-block', item_input_group, 'btnItemInput', 'Add Item');
+        }
+
+        var renderInputFeilds = function () {
             removeAllInputfeild();
-            var inputFeildCounter=storage.getInputFeildCountValue();
-            for(var i=0;i<inputFeildCounter;i++){
+            var inputFeildCounter = storage.getInputFeildCountValue();
+            for (var i = 0; i < inputFeildCounter; i++) {
                 addInputfeild(i);
             }
             renderInputButtons();
@@ -70,121 +70,145 @@
 
 
 
-        var buttonDisableEnable=function(){
-            if(storage.getInputFeildCountValue()<2){
+        var buttonDisableEnable = function () {
+            if (storage.getInputFeildCountValue() < 2) {
                 disableButton(btnRemoveInput);
                 console.log("Butotn Should be disabled");
-            }else{
+            } else {
                 enableButton(btnRemoveInput);
                 console.log("Butotn Should be Enable");
             }
 
-            if(isEditMode){
+            if (isEditMode) {
                 disableButton(btnAddInput);
                 disableButton(btnRemoveInput);
             }
         }
-        
-        return{
-            'renderInputFeilds':renderInputFeilds,
-            'buttonDisableEnable':buttonDisableEnable
+
+        return {
+            'renderInputFeilds': renderInputFeilds,
+            'buttonDisableEnable': buttonDisableEnable
         }
     }
 
-    var StorageFuncitons=function(){
+    var StorageFuncitons = function () {
 
-        var readFromStorage=function(key){
-            var data=localStorage.getItem(key);
-            if(data!=null)
+        var readFromStorage = function (key) {
+            var data = localStorage.getItem(key);
+            if (data != null)
                 return data;
-            else 
+            else
                 return null;
         }
 
-        var writeToStorage=function(key,data){
-            localStorage.setItem(key,data);
+        var writeToStorage = function (key, data) {
+            localStorage.setItem(key, data);
         }
 
-        var initializeInputFeildCounter=function(){
-            writeToStorage('inputFeildCounter','1');
-        }   
+        var initializeInputFeildCounter = function () {
+            writeToStorage('inputFeildCounter', '1');
+        }
 
-        var getInputFeildCountValue=function(){
+        var getInputFeildCountValue = function () {
             return readFromStorage('inputFeildCounter');
         }
 
-        var incrementInputFeildCounter=function(){
-            var counter=getInputFeildCountValue();
-            writeToStorage('inputFeildCounter',++counter);
+        var incrementInputFeildCounter = function () {
+            var counter = getInputFeildCountValue();
+            writeToStorage('inputFeildCounter', ++counter);
         }
 
-        var decrementInputFeildCounter=function(){
-            var counter=getInputFeildCountValue();
-            writeToStorage('inputFeildCounter',--counter);
+        var decrementInputFeildCounter = function () {
+            var counter = getInputFeildCountValue();
+            writeToStorage('inputFeildCounter', --counter);
         }
 
-        return{
-            'initializeInputFeildCounter':initializeInputFeildCounter,
-            'getInputFeildCountValue':getInputFeildCountValue,
-            'incrementInputFeildCounter':incrementInputFeildCounter,
-            'decrementInputFeildCounter':decrementInputFeildCounter
+        var addNewRowToStorage=function(values){
+            var previousValues=readFromStorage('table-items');
+            if(previousValues){
+                //find out last Id of the row
+                //create new obejct with that id and values
+                //write to storage    
+            }else{
+                //set id to 1
+                //create new objet with that id and values
+                //write to storage
+            }
+        }
+
+        return {
+            'initializeInputFeildCounter': initializeInputFeildCounter,
+            'getInputFeildCountValue': getInputFeildCountValue,
+            'incrementInputFeildCounter': incrementInputFeildCounter,
+            'decrementInputFeildCounter': decrementInputFeildCounter,
+            'addNewRowToStorage':addNewRowToStorage
         }
 
     }
 
-    var Events=function(){
-       
-        var btnAddInputEvent=function(){
+    var Events = function () {
+
+        var btnAddInputEvent = function () {
             storage.incrementInputFeildCounter();
             render.renderInputFeilds();
             render.buttonDisableEnable();
             console.log("Add Input Evenet fired");
         }
 
-        var btnRemoveInputEvent=function(){
+        var btnRemoveInputEvent = function () {
             storage.decrementInputFeildCounter();
             render.renderInputFeilds();
             render.buttonDisableEnable();
             console.log("Remove Input Evenet fired");
         }
 
-        var btnItemInputEvent=function(){
+        var btnItemInputEvent = function () {
+            var inputValues = [];
+            var size=inputFeilds.length;
+            for(let i=0;i<size;i++){
+               inputValues.push(inputFeilds[i].value);
+            }
+            storage.addNewRowToStorage(inputValues);
+            console.log(inputValues);
             console.log("Item Input event fired");
         }
         return {
-            'btnAddInputEvent':btnAddInputEvent,
-            'btnRemoveInputEvent':btnRemoveInputEvent,
-            'btnItemInputEvent':btnItemInputEvent
+            'btnAddInputEvent': btnAddInputEvent,
+            'btnRemoveInputEvent': btnRemoveInputEvent,
+            'btnItemInputEvent': btnItemInputEvent
         };
     }
 
-    vm.domBinder=function(){
-        btnAddInput=vm.getElementById('btnAddInput');
-        btnRemoveInput=vm.getElementById('btnRemoveInput');
-        btnItemInput=vm.getElementById('btnItemInput');
+    //DOM BINDING
+    vm.domBinder = function () {
+        inputFeilds = vm.getElementsByClassName('inputFieldItem');
+        btnAddInput = vm.getElementById('btnAddInput');
+        btnRemoveInput = vm.getElementById('btnRemoveInput');
+        btnItemInput = vm.getElementById('btnItemInput');
     }
 
-    vm.attachEventListener=function(){
+    //EVENT ATTACHING
+    vm.attachEventListener = function () {
         btnAddInput.addEventListener('click', events.btnAddInputEvent);
         btnRemoveInput.addEventListener('click', events.btnRemoveInputEvent);
-       // btnItemInput.addEventListener('click',events.btnItemInputEvent);
+        btnItemInput.addEventListener('click', events.btnItemInputEvent);
     }
 
-    function init(){
-        render=new vm.RenderFunctions();
-        events=new Events();
-        storage=new StorageFuncitons();
+    function init() {
+        render = new vm.RenderFunctions();
+        events = new Events();
+        storage = new StorageFuncitons();
 
         //Initializing StorageData
-        if(storage.getInputFeildCountValue()==null){
+        if (storage.getInputFeildCountValue() == null) {
             storage.initializeInputFeildCounter();
             console.log("storage Initialized");
         }
-      
+
         //Render Dynamic Displayts
         render.renderInputFeilds();
         vm.domBinder();
-     
+
         vm.attachEventListener();
     }
 
